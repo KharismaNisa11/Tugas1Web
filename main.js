6f2ec5346cd94833c27563e7aaedb7218b8a914b
Latihan.2 <rismanissa17@gmail.com>
2023-11-07 23:06:11 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container .btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header__container a", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+// service container
+ScrollReveal().reveal(".skill__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// projects container
+ScrollReveal().reveal(".hobby__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// banner container
+ScrollReveal().reveal(".banner__content", scrollRevealOption);
+
+// blogs container
+ScrollReveal().reveal(".about__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
